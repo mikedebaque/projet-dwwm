d0ec89416d019296c12f37d284886a0fafbc94b6
ligonniere_alexie <alexie.ligonniere@gmail.com>
2019-10-18 10:33:25 +0200

    ajout mot de passe oublié et modification mot de passe

---
+++ modification_mot_de_passe/modifiaction_mot_de_passe.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="modification_mot_de_passe.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        
+        <div class="mapage">
+                <div class="jumbotron jumbotron-fluid border" id="header">
+                        <div class="container">
+                                <div class=center-block> 
+                                    <h1 class="text-center">header </h1>
+                                </div>
+                        </div>
+                </div>
+<!-- -------------------------------------------------------------------------cadre--------------------------------------------------------------------- -->
+                <div class="row" id="espace4"></div>
+
+                <div class="row">
+                    <div class="container body-content border" id="block">
+                        <div class="center-block" style="width:vw;margin-top:80px;">
+
+                            <h2 class="text-center">Modifier votre mot de passe:</h2>
+
+                                <div class="row" id="espace1"></div>
+                    <form action="">
+                                <div class="form-row">
+                                        <div class="form-group col-lg-6 offset-lg-3">
+                                            <label for="mtpasse">Ancien mot de passe.</label>
+                                            <input type="password" class="form-control" id="mtpasse" placeholder="Ancien mot de passe">
+                                        </div>
+                                </div>
+
+                        <div class="row" id="espace2"></div>
+
+                                <div class="form-row">
+                                        <div class="form-group col-lg-6 offset-lg-3">
+                                            <label for="Nmtpasse">Nouveau mot de passe.</label>
+                                            <input type="password" class="form-control" id="Nmtpasse" placeholder="Nouveau mot de passe.">
+                                        </div>
+                                </div>
+
+                        <div class="row" id="espace2"></div>
+
+                                <div class="form-row">
+                                        <div class="form-group col-lg-6 offset-lg-3">
+                                            <label for="CNmtpasse">Confirmer nouveau mot de passe.</label>
+                                            <input type="password" class="form-control" id="CNmtpasse" placeholder="Confirmer nouveau mot de passe">
+                                        </div>
+                                </div>
+
+                        <div class="row" id="espace2"></div>
+
+                    
+                                <div class="form-row">
+                                    <div class="offset-lg-4">
+                                        <button type="button" class="btn btn-outline-danger btn-lg">Annuler</button>
+                                    </div>
+                                    <div class="offset-lg-1">
+                                        <button type="button" class="btn btn-outline-success btn-lg">Envoyer</button>
+                                    </div>
+                                </div> 
+
+                    </form>
+                    
+
+                                    
+                            
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <script src="" async defer></script>
+    </body>
+</html>
---
+++ mot_de_passe_oublie/mot_de_passe_oublie.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="mot_de_passe_oublie.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+     <div class="mapage">
+      <div class="jumbotron jumbotron-fluid border" id="header">
+        <div class="container">
+                <div class=center-block> 
+                    <h1 class="text-center">header </h1>
+                </div>
+        </div>
+      </div>
+      <div class="row" id="espace4"></div>
+    
+       <!-- ----------------------------------------------------------------cadre-------------------------------------------------------------------- -->
+      <div class="row">
+        <div class="container body-content border" id="block">
+            <div class="center-block" style="width:px;margin-top:80px;">
+
+                <div class="my-4">
+                    <h2 class="text-center">  Pas de panique on s'occupe de tout! </h2> 
+                </div>
+
+                <div class="row" id="espace1"></div>
+
+                <div class="my-2">
+                    <h3 class="text-center font-weight-normal">Renseignez votre e-mail pour renouveler votre mot de passe.</h3>
+                </div>
+
+                <div class="row" id="espace2"></div>
+
+            <form action="">
+                <div class="mx-auto">
+
+                    <div class="form-row">
+                        <div class="form-group col-lg-6 offset-lg-3">
+                            <label for="email"></label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="email@example.com">
+                            <small id="emailHelp" class="form-text text-muted">Nous ne partagerons jamais votre email avec qui que ce soit.</small>
+                        </div>
+                    </div>
+                            <div class="row" id="espace2"></div>
+
+                    <div class="form-row">
+                        <div class="offset-lg-4">
+                        <button type="button" class="btn btn-outline-danger btn-lg">Annuler</button>
+                        </div>
+                        <div class="offset-lg-1">
+                        <button type="button" class="btn btn-outline-success btn-lg">Envoyer</button>
+                        </div>
+                    </div> 
+
+                </div>  
+            </form>
+
+            <div class="row" id="espace3"></div>
+            
+            </div>
+        </div>
+        </div>
+        <div class="row" id="espace4"></div>
+        <!-- ------------------------------------------------------------------footer------------------------------------------------------------------------------ -->
+            <footer class="page-footer font-small blue border">
+                <div class="footer-copyright text-center py-3">
+                    © 2019 Copyright:
+                  </div>
+            </footer>
+     </div>
+        <script src="" async defer></script>
+    </body>
+</html>
