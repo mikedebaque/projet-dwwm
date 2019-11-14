ad48d4a4f2388189c2c3879afcada1b6bf598a48
Stephane <stephane.poirrier@yahoo.com>
2019-11-14 20:26:11 +0100

    Footer intégrable PHP

--- Footer/Footer.php
+++ Footer/Footer.php
@@ -1,27 +1,28 @@
+<?php 
+echo "
 
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="Footer.css">
+<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+<link rel='stylesheet' href='Footer.css'>
 
 <!-- Footer -->
-<footer class="page-footer font-small bg-secondary text-white dbg-primary pt-4">
-    <div class="container-fluid text-center text-md-left">
-        <div class="row">
-          <!--  <div class="col-md-3 mt-md-0 mt-3"> 
+<footer class='page-footer font-small bg-secondary text-white dbg-primary pt-4'>
+    <div class='container-fluid text-center text-md-left'>
+        <div class='row'>
+          <!--  <div class='col-md-3 mt-md-0 mt-3'> 
   
-          <h5 class="text-uppercase">HORAIRES</h5>
+          <h5 class='text-uppercase'>HORAIRES</h5>
           <p>A propos de nous</p><br> 
   
         </div> -->
 
         <!-- Grid column -->
   
-        <hr class="clearfix w-100 d-md-none pb-3">
+        <hr class='clearfix w-100 d-md-none pb-3'>
 
-        <div class="col-md-4 mb-md-4 mb-4">
-          <h5 class="text-uppercase">HORAIRES D'OUVERTURE</h5>
-          <hr class="bg-white accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 50%";> <!-- Code récupéré & modif qui permet de recréer un effet de souligné -->
-          <ul class="list-unstyled">
+        <div class='col-md-4 mb-md-4 mb-4'>
+          <h5 class='text-uppercase'>HORAIRES D'OUVERTURE</h5>
+          <hr class='bg-white accent-3 mb-4 mt-0 d-inline-block mx-auto' style='width: 50%';> <!-- Code récupéré & modif qui permet de recréer un effet de souligné -->
+          <ul class='list-unstyled'>
                 <li>
                 <p> Lundi 08h - 19h </p>
                 </li>
@@ -44,23 +45,23 @@
         </div>
 
         <!-- Grid column -->
-        <div class="col-md-2 mb-md-0 mb-2">
-          <h5 class="text-uppercase">Lorem Ipsum</h5>
-          <hr class="bg-white -3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60%";>
+        <div class='col-md-2 mb-md-0 mb-2'>
+          <h5 class='text-uppercase'>Lorem Ipsum</h5>
+          <hr class='bg-white -3 mb-4 mt-0 d-inline-block mx-auto' style='width: 60%';>
         
-          <ul class="list-unstyled">
+          <ul class='list-unstyled'>
             <li>
-                <!-- le "#!" permet de créer un faux lien : on reste sur la page si on clic dessus -->
-            <a href="#!"> Notre société </a>
+                <!-- le '#!' permet de créer un faux lien : on reste sur la page si on clic dessus -->
+            <a href='#!'> Notre société </a>
             </li>
             <li>
-              <a href="#!"> Nos partenaires </a>
+              <a href='#!'> Nos partenaires </a>
             </li>
             <li>
-              <a href="#!"> Conditions et services </a>
+              <a href='#!'> Conditions et services </a>
             </li>
             <li>
-              <a href="#!"> Lorem Ipsum </a>
+              <a href='#!'> Lorem Ipsum </a>
             </li>
           </ul>
   
@@ -68,40 +69,40 @@
         <!-- column -->
   
         <!-- column -->
-        <div class="col-md-3 mb-md-0 mb-3">
+        <div class='col-md-3 mb-md-0 mb-3'>
   
           <!-- Menu 02 -->
-          <h5 class="text-uppercase">Lorem Ipsum</h5>
-          <hr class="bg-white accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 40%";>
+          <h5 class='text-uppercase'>Lorem Ipsum</h5>
+          <hr class='bg-white accent-3 mb-4 mt-0 d-inline-block mx-auto' style='width: 40%';>
   
-          <ul class="list-unstyled">
+          <ul class='list-unstyled'>
             <li>
-              <a href="#!"> Lorem Ipsum </a>
+              <a href='#!'> Lorem Ipsum </a>
             </li>
             <li>
-              <a href="#!"> Lorem Ipsum </a>
+              <a href='#!'> Lorem Ipsum </a>
             </li>
             <li>
-              <a href="#!"> Lorem Ipsum </a>
+              <a href='#!'> Lorem Ipsum </a>
             </li>
             <li>
-              <a href="#!"> Lorem Ipsum </a>
+              <a href='#!'> Lorem Ipsum </a>
             </li>
           </ul>
   
         </div>
 
         <!-- Les infos de contact tout à droite -->
-        <div class="col-md-3 mb-md-0 mb-3">
-            <ul class="list-unstyled">
+        <div class='col-md-3 mb-md-0 mb-3'>
+            <ul class='list-unstyled'>
                 <li>
-                   <img src="icon_tel.png" class="img-fluid rounded float-left" alt="Tel icon responsive" style="width:09%"><p> Numéro de téléphone</p>
+                   <img src='icon_tel.png' class='img-fluid rounded float-left' alt='Tel icon responsive' style='width:09%'><p> Numéro de téléphone</p>
                 </li>
                 <li>
-                        <img src="icon_email.png" class="img-fluid rounded float-left" alt="Email icon responsive" style="width:09%"><p> Email du drive</p>
+                        <img src='icon_email.png' class='img-fluid rounded float-left' alt='Email icon responsive' style='width:09%'><p> Email du drive</p>
                 </li>
                 <li>
-                        <img src="icon_car.png" class="img-fluid rounded float-left" alt="Address icon responsive" style="width:11%"><p>Adresse du Drive</p>
+                        <img src='icon_car.png' class='img-fluid rounded float-left' alt='Address icon responsive' style='width:11%'><p>Adresse du Drive</p>
                 </li>
             </ul>
         </div>
@@ -114,5 +115,7 @@
     
 
     <!-- Copyright -->
-    <div class="footer-copyright text-center bg-dark py-3">© 2019 Copyright</div>  
-  </footer>
+    <div class='footer-copyright text-center bg-dark py-3'>© 2019 Copyright</div>  
+  </footer> ";
+
+  ?>
