3dd3f7939b17dddd9b930ac4deb3f7452e22772b
mikedebaque <supergothman@hotmail.fr>
2019-11-18 16:29:23 +0100

    update autoComplete

--- js/autoCompleteAdresse.js
+++ js/autoCompleteAdresse.js
@@ -16,28 +16,32 @@ $('#adresseInscription').on('keydown',function(e){
         return false;
     }
 
-    apiurl = 'https://api-adresse.data.gouv.fr/search/?q='+this.value;
+    if(testWord.length >= 3)
+    {
+        apiurl = 'https://api-adresse.data.gouv.fr/search/?q='+this.value;
         
-    var p = callAjax(apiurl,'get');
-    p.done(function(data){
-        console.log(data.features);
-        $('#inputAdresse').children('div').remove();
-        data.features.forEach(element => {
-          $('<div>').attr({'id':element.properties.id}).text(element.properties.label).appendTo($('#inputAdresse'));
-          $('#'+element.properties.id).on('mouseover',function(){
-              $(this).css({'background-color':'#2795f5'});
-          })
-          $('#'+element.properties.id).on('mouseout',function(){
-              $(this).css({'background-color': 'white'});
-          })
-          $('#'+element.properties.id).on('click',function(){
-              $('#adresseInscription').val(element.properties.name);
-              $('#cpInscription').val(element.properties.postcode);
-              $('#villeInscription').val(element.properties.city);
-              $('#inputAdresse').children('div').remove();
-          })
+        var p = callAjax(apiurl,'get');
+        p.done(function(data){
+            console.log(data.features);
+            $('#inputAdresse').children('div').remove();
+            data.features.forEach(element => {
+            $('<div>').attr({'id':element.properties.id}).text(element.properties.label).appendTo($('#inputAdresse'));
+            $('#'+element.properties.id).on('mouseover',function(){
+                $(this).css({'background-color':'#2795f5'});
+            })
+            $('#'+element.properties.id).on('mouseout',function(){
+                $(this).css({'background-color': 'white'});
+            })
+            $('#'+element.properties.id).on('click',function(){
+                $('#adresseInscription').val(element.properties.name);
+                $('#cpInscription').val(element.properties.postcode);
+                $('#villeInscription').val(element.properties.city);
+                $('#inputAdresse').children('div').remove();
+            })
+            })
         })
-    })
+    }
+    
     if(this.value=='')
     {
         $('#inputAdresse').children('div').remove();  
